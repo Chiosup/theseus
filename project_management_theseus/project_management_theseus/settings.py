9bc164b5d20d50fdc041dc0dea7f1ab69a2324aa
Chiosup <golubev.stepan2003@gmail.com>
2025-02-13 13:49:41 +0300

    redact users

--- project_management_theseus/project_management_theseus/settings.py
+++ project_management_theseus/project_management_theseus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,10 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'users.CustomUser'  # Настроим кастомную модель пользователя
 
+LOGIN_REDIRECT_URL = 'index'  # Куда перекидывать после логина
+LOGOUT_REDIRECT_URL = 'login'  # Куда перекидывать после выхода
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -116,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- project_management_theseus/users/views.py
+++ project_management_theseus/users/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'users/index.html')
+
+class CustomLoginView(LoginView):
+    template_name = 'users/login.html'
+
+class CustomLogoutView(LogoutView):
+    def get(self, request, *args, **kwargs):
+        return redirect('login')
